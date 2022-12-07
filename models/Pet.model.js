f9937c003eacf30cb1896c06807630af44e82a8d
Frances Morales Velilla <frances0688@gmail.com>
2022-12-07 14:03:28 +0100

    Initial commit- create folder structure, db configuration,  user and pet models, and auth routes

---
+++ models/Pet.model.js
@@ -0,0 +1,70 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const petSchema = new Schema
+    ({
+        name: String,
+        imgPath: {
+            type:String,
+            default:'https://www.petcloud.com.au/img/pet_placeholder.png',
+        },
+        typeOfPet: {
+            type: String,
+            enum: ['dog','cat', 'other'],
+        },
+        weight: Number,    
+        ageYears: Number,
+        ageMonths: Number,
+        gender: {
+            type: String,
+            enum: ['male','female'],
+        }, 
+        breed: String,
+        microchipped: Boolean,
+        spayedOrNeutered: Boolean,
+        houseTrained:{
+            type: String,
+            enum: ['yes', 'no', 'unsure', 'depends'],
+        },
+        friendlyWithDogs: Boolean,
+        friendlyWithCats: Boolean,
+        about: String,
+        pottySchedule: {
+            type: String,
+            enum: ['every hour', 'every 2 hours', 'every 4 hours', 'every 8 hours', 'custom'],
+        },
+        energy: {
+            type: String,
+            enum: ['high', 'moderate', 'low'],
+        },
+        feedingSchedule: {
+            type: String,
+            enum: ['morning', 'twice a day', 'custom'],
+        }, 
+        canBeAlone: {
+            type: String,
+            enum: ['less than 1 hour', '1 - 4 hours', '4 - 8 hours', 'custom'],
+        }, 
+        medication: String,
+        otherCareInfo: String,
+        vetName: String,
+        vetNumber: Number,
+        vetStreet: String,
+        vetCity: String,
+        vetState: String,
+        vetZip: Number,
+        additionalVetInfo: String,
+        photo: [String],
+        active: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: String,
+            required: true
+        }
+});
+
+const Pet = model("Pet", petSchema);
+
+module.exports = Pet;
---
+++ models/User.model.js
@@ -0,0 +1,56 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      default: "user"
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    imgPath: {
+      type: String,
+      default: "https://camo.githubusercontent.com/c6fe2c13c27fe87ac6581b9fe289d2f071bd1b4ef6f3e3c5fc2aba0bbc23fd88/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f372f37632f50726f66696c655f6176617461725f706c616365686f6c6465725f6c617267652e706e67"
+    },
+    dob: {
+      type: Date,
+      required: true
+    },
+    phone: String,
+    addressStreet: String,
+    addressCity: String, 
+    addressState: String,
+    addressZip: Number,
+    emergencyContactName: String ,
+    emergencyContactPhone: String,
+    pets: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Pet'
+    }]
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
