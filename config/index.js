6f79a89e66454bfcd484cf1aa4e93d1ef82fdc96
Frances Morales Velilla <frances0688@gmail.com>
2022-12-16 10:17:51 +0100

    Config file correction

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = (app) => {
   // controls a very specific header to pass headers from the frontend
   app.use(
     cors({
-      origin: [process.env.FRONTEND_URL],
+      origin: [process.env.FRONTEND_URL, "http://localhost:3000"],
     })
   );
 
